181c3c4086ebfe40d542007a8fe616d9ee6e3d91
Bruno Souto <bcovies@gmail.com>
2021-07-19 19:12:49 -0300

    Adicionado

--- instalador_chocolatey.ps1
+++ instalador_chocolatey.ps1
@@ -9,6 +9,7 @@ Write-Host $ehAdministrador
 
 if ($ehAdministrador  ) {
     Write-Host "Modo de administrador ativo, prosseguindo."
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 else {
     Write-Host "Para instalação dos aplicativos é necessária a inicialização do powershell em modo administrativo."
