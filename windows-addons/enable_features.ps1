2281ecba7543d5c003d23404118f8a25c95c4afb
Bruno Souto <bcovies@gmail.com>
2022-03-14 22:02:43 -0300

    Adicionado features

--- windows-addons/enable_features.ps1
+++ windows-addons/enable_features.ps1
@@ -4,7 +4,18 @@ Start-Process powershell -Verb #Requires -RunAsAdministrator
 #Requires -RunAsAdministrator
 Enable-WindowsOptionalFeature -Online -FeatureName "TelnetClient" -All
 Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "HypervisorPlatform" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "VirtualMachinePlatform" -All
 Enable-WindowsOptionalFeature -Online -FeatureName "Containers-DisposableClientVM" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "Containers" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "NFS-Administration" -All
 Enable-WindowsOptionalFeature -Online -FeatureName "Windows-Defender-ApplicationGuard" -All
-
+Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-All" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Tools-All" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Management-PowerShell" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Hypervisor" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Services" -All
+Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Hyper-V-Management-Clients" -All
+ 
 Read-Host "Finalizado. Pressione qualquer tecla para sair..."
